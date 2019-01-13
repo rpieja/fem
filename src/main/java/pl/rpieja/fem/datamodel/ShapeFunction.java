0d16f935d16add915073bab2f35cf4803b6e304e
Radoslaw Pieja <pieja.radoslaw@gmail.com>
2019-01-13 13:37:32 +0100

    Add shape functions and SF derivatives
    Signed-off-by: Radoslaw Pieja <pieja.radoslaw@gmail.com>

--- src/main/java/pl/rpieja/fem/datamodel/ShapeFunction.java
+++ src/main/java/pl/rpieja/fem/datamodel/ShapeFunction.java
@@ -2,9 +2,52 @@ package pl.rpieja.fem.datamodel;
 
 public class ShapeFunction {
 
-
-    private double baseFunction(){
-
-        return 0.1;
+    public static double N1(double ksi, double eta) {
+        return 0.25 * (1 - ksi) * (1 - eta);
     }
+
+    public static double N2(double ksi, double eta) {
+        return 0.25 * (1 + ksi) * (1 - eta);
+    }
+
+    public static double N3(double ksi, double eta) {
+        return 0.25 * (1 + ksi) * (1 + eta);
+    }
+
+    public static double N4(double ksi, double eta) {
+        return 0.25 * (1 - ksi) * (1 + eta);
+    }
+
+    public static double dN1dKsi(double eta) {
+        return -0.25 * (1 - eta);
+    }
+
+    public static double dN2dKsi(double eta) {
+        return 0.25 * (1 - eta);
+    }
+
+    public static double dN3dKsi(double eta) {
+        return 0.25 * (1 + eta);
+    }
+
+    public static double dN4dKsi(double eta) {
+        return -0.25 * (1 + eta);
+    }
+
+    public static double dN1dEta(double ksi) {
+        return -0.25 * (1 - ksi);
+    }
+
+    public static double dN2dEta(double ksi) {
+        return -0.25 * (1 + ksi);
+    }
+
+    public static double dN3dEta(double ksi) {
+        return 0.25 * (1 + ksi);
+    }
+
+    public static double dN4dEta(double ksi) {
+        return 0.25 * (1 - ksi);
+    }
+
 }
